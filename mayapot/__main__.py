d4320139ffb60fef277c735716f6486192a8cbc9
InTack2 <takumi236@gmail.com>
2020-06-17 21:35:06 +0900

    現状追加

--- mayapot/__main__.py
+++ mayapot/__main__.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" For command line execution.
+"""
+For command line execution.
 Todo:
     * create test comand line.
     * create build for ci tool(goal).
     * create function to open fbx files.
     * create function to get object list.
 """
+import os
+import subprocess
+
 import click
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+MAYA_SCRIPTS_PATH = os.path.join(SCRIPT_PATH, "maya")
 
 
 @click.group()
@@ -17,12 +24,17 @@ def cmd():
 
 @cmd.command()
 def get_object_list():
-    click.echo("maya ls command.")
+    test_command = os.path.join(MAYA_SCRIPTS_PATH, "get_object_list.py")
+    subprocess.call(['mayapy', test_command])
 
 
 @cmd.command()
 def change_soft_edge():
-    click.echo("all object soft edges.")
+    test_command = os.path.join(MAYA_SCRIPTS_PATH, "change_soft_edge.py")
+    subprocess.call(['mayapy', test_command])
+    # print(result)
+    # subprocess.Popen(["mayabach", "-command", "{}".format(exec_file)])
+    #click.echo("all object soft edges.")
 
 
 def main():
@@ -31,3 +43,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"pipenv run python -m mayapot get-object-list"
---
+++ mayapot/maya/get_object_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+"""
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import generators
+from __future__ import division
+from maya import standalone
+import maya.cmds as cmds
+standalone.initialize("python")
+print(cmds.ls(type="transform"))
+standalone.uninitialize("python")
